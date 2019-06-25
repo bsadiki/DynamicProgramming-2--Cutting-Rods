9325320986dd5ad4eb16312f0a92d831732182f3
bsadiki <pedrosadiki@gmail.com>
2019-06-25 13:56:39 +0100

    dynamic programing solution

--- src/com/xmart/solution/Solution.java
+++ src/com/xmart/solution/Solution.java
@@ -2,17 +2,21 @@ package com.xmart.solution;
 
 public class Solution {
     public static void main(String[] args) {
-        int prices[] = {1,5,8,9,10};
+        int prices[] = {1, 5, 8, 9, 10};
         System.out.println(maximumRevenue(5, prices));
     }
 
-    static int maximumRevenue(int n, int[] prices) {
+    private static int maximumRevenue(int n, int[] prices) {
         if (prices.length == 0)
             return 0;
-        int max = 0;
-        for (int i = 0; i < n; i++) {
-            max = Math.max(max,prices[i] + maximumRevenue(n-1-i, prices));
+        int[] maxForEachN = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            int maxForI = 0;
+            for (int j = 0; j < i; j++) {
+                maxForI = Math.max(maxForI, prices[j] + maxForEachN[i - 1 - j]);
+            }
+            maxForEachN[i] = maxForI;
         }
-        return max;
+        return maxForEachN[n];
     }
 }
