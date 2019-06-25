1538684adbc796e2965298c5839e1e2344a77b4a
bsadiki <pedrosadiki@gmail.com>
2019-06-25 12:31:52 +0100

    naive solution : Recursive

--- src/com/xmart/solution/Solution.java
+++ src/com/xmart/solution/Solution.java
@@ -2,7 +2,17 @@ package com.xmart.solution;
 
 public class Solution {
     public static void main(String[] args) {
-
+        int prices[] = {1,5,8,9,10};
+        System.out.println(maximumRevenue(5, prices));
     }
 
+    static int maximumRevenue(int n, int[] prices) {
+        if (prices.length == 0)
+            return 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max,prices[i] + maximumRevenue(n-1-i, prices));
+        }
+        return max;
+    }
 }
